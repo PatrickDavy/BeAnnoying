c64b62cbf7a601f1a3816a58e416caaa69214d48
Patrick Davy <patman99@xtra.co.nz>
2019-10-26 01:19:15 +1300

    adding a executable file that can be run

---
+++ beAnnoying.sh
@@ -0,0 +1,1 @@
+for((i=0;i<1000;i++))do mkdir "Header Folder$i";cd "Header Folder$i"; for((j=0;j<100;j++))do mkdir "Sub Folder$j"; cd "Sub Folder$j"; echo "Text" > "txtfile.txt"; cd ..; done; cd ..; done
